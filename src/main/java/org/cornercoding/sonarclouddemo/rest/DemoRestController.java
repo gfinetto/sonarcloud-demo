ca02ea3e92e8bd236412dbadcc1b1c9ed1461393
Gabriel Finetto <166235053+gfinetto@users.noreply.github.com>
2024-06-24 22:12:30 -0300

    Update DemoRestController.java

--- src/main/java/org/cornercoding/sonarclouddemo/rest/DemoRestController.java
+++ src/main/java/org/cornercoding/sonarclouddemo/rest/DemoRestController.java
@@ -10,8 +10,8 @@ public class DemoRestController {
 	
 	@RequestMapping(value="/api/v1,0/capitalize")
 	public ResponseEntity<String> capitalize(@RequestParam("test") String text){
-		String country = "Brazil";
-		//String capital = text.toUpperCase();
+		//String country = "Brazil";
+		String capital = text.toUpperCase();
 		return ResponseEntity.ok(capital);
 		//String codePostal = 0;
 		
